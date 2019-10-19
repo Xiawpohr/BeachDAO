3618f40a9b30a0b9662599653df70b6da0c48776
Ron <ande1218@gmail.com>
2019-10-19 15:55:33 +0800

    feat (init): run without web3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import Web3Provider from 'web3-react'
-import Web3 from 'web3'
+// import Web3Provider from 'web3-react'
+// import Web3 from 'web3'
 import ApplicationContextProvider, {
   Updater as ApplicationContextUpdater,
 } from './contexts/application'
 import TransactionContextProvider, {
   Updater as TransactionContextUpdater,
 } from './contexts/transaction'
-import Web3Manager from './components/Web3Manager'
+// import Web3Manager from './components/Web3Manager'
 import Home from './pages/Home'
-import NetworkOnlyConnector from './connectors/NetworkOnlyConnector'
+// import NetworkOnlyConnector from './connectors/NetworkOnlyConnector'
 import InjectedConnector from './connectors/InjectedConnector'
 
-const Network = new NetworkOnlyConnector({
-  providerURL: process.env.REACT_APP_NETWORK_URL || '',
-})
-const Injected = new InjectedConnector({
-  supportedNetworks: [Number(process.env.REACT_APP_NETWORK_ID || '1')],
-})
-const connectors = { Injected, Network }
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+  }
+`
+// const Network = new NetworkOnlyConnector({
+//   providerURL: process.env.REACT_APP_NETWORK_URL || '',
+// })
+// const Injected = new InjectedConnector({
+//   supportedNetworks: [Number(process.env.REACT_APP_NETWORK_ID || '1')],
+// })
+// const connectors = { Injected, Network }
 
 function ContextProviders({ children }) {
   return (
@@ -39,14 +46,10 @@ function Updaters() {
 
 function App() {
   return (
-    <Web3Provider connectors={connectors} libraryName='web3.js' web3Api={Web3}>
-      <ContextProviders>
-        <Updaters />
-        <Web3Manager>
-          <Home />
-        </Web3Manager>
-      </ContextProviders>
-    </Web3Provider>
+    <div>
+      <GlobalStyle />
+      <Home />
+    </div>
   )
 }
 
--- src/contexts/transaction.js
+++ src/contexts/transaction.js
@@ -1,209 +1,209 @@
-import React, {
-  createContext,
-  useContext,
-  useReducer,
-  useMemo,
-  useCallback,
-  useEffect,
-} from 'react'
-import { useWeb3Context } from 'web3-react'
-import { safeAccess } from '../utils'
-import { useBlockNumber } from './application'
+// import React, {
+//   createContext,
+//   useContext,
+//   useReducer,
+//   useMemo,
+//   useCallback,
+//   useEffect,
+// } from 'react'
+// import { useWeb3Context } from 'web3-react'
+// import { safeAccess } from '../utils'
+// import { useBlockNumber } from './application'
 
-const RESPONSE = 'response'
-const CUSTOM_DATA = 'CUSTOM_DATA'
-const BLOCK_NUMBER_CHECKED = 'BLOCK_NUMBER_CHECKED'
-const RECEIPT = 'receipt'
+// const RESPONSE = 'response'
+// const CUSTOM_DATA = 'CUSTOM_DATA'
+// const BLOCK_NUMBER_CHECKED = 'BLOCK_NUMBER_CHECKED'
+// const RECEIPT = 'receipt'
 
-const ADD = 'ADD'
-const CHECK = 'CHECK'
-const FINALIZE = 'FINALIZE'
+// const ADD = 'ADD'
+// const CHECK = 'CHECK'
+// const FINALIZE = 'FINALIZE'
 
-const TransactionsContext = createContext()
+// const TransactionsContext = createContext()
 
-export function useTransactionsContext() {
-  return useContext(TransactionsContext)
-}
+// export function useTransactionsContext() {
+//   return useContext(TransactionsContext)
+// }
 
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ADD: {
-      const { networkId, hash, response } = payload
+// function reducer(state, { type, payload }) {
+//   switch (type) {
+//     case ADD: {
+//       const { networkId, hash, response } = payload
 
-      if (safeAccess(state, [networkId, hash]) !== null) {
-        throw Error('Attempted to add existing transaction.')
-      }
+//       if (safeAccess(state, [networkId, hash]) !== null) {
+//         throw Error('Attempted to add existing transaction.')
+//       }
 
-      return {
-        ...state,
-        [networkId]: {
-          ...(safeAccess(state, [networkId]) || {}),
-          [hash]: {
-            [RESPONSE]: response,
-          },
-        },
-      }
-    }
-    case CHECK: {
-      const { networkId, hash, blockNumber } = payload
+//       return {
+//         ...state,
+//         [networkId]: {
+//           ...(safeAccess(state, [networkId]) || {}),
+//           [hash]: {
+//             [RESPONSE]: response,
+//           },
+//         },
+//       }
+//     }
+//     case CHECK: {
+//       const { networkId, hash, blockNumber } = payload
 
-      if (safeAccess(state, [networkId, hash]) === null) {
-        throw Error('Attempted to check non-existent transaction.')
-      }
+//       if (safeAccess(state, [networkId, hash]) === null) {
+//         throw Error('Attempted to check non-existent transaction.')
+//       }
 
-      return {
-        ...state,
-        [networkId]: {
-          ...(safeAccess(state, [networkId]) || {}),
-          [hash]: {
-            ...(safeAccess(state, [networkId, hash]) || {}),
-            [BLOCK_NUMBER_CHECKED]: blockNumber,
-          },
-        },
-      }
-    }
-    case FINALIZE: {
-      const { networkId, hash, receipt } = payload
+//       return {
+//         ...state,
+//         [networkId]: {
+//           ...(safeAccess(state, [networkId]) || {}),
+//           [hash]: {
+//             ...(safeAccess(state, [networkId, hash]) || {}),
+//             [BLOCK_NUMBER_CHECKED]: blockNumber,
+//           },
+//         },
+//       }
+//     }
+//     case FINALIZE: {
+//       const { networkId, hash, receipt } = payload
 
-      if (safeAccess(state, [networkId, hash]) === null) {
-        throw Error('Attempted to finalize non-existent transaction.')
-      }
+//       if (safeAccess(state, [networkId, hash]) === null) {
+//         throw Error('Attempted to finalize non-existent transaction.')
+//       }
 
-      return {
-        ...state,
-        [networkId]: {
-          ...(safeAccess(state, [networkId]) || {}),
-          [hash]: {
-            ...(safeAccess(state, [networkId, hash]) || {}),
-            [RECEIPT]: receipt,
-          },
-        },
-      }
-    }
-    default: {
-      throw Error(
-        `Unexpected action type in TransactionsContext reducer: '${type}'.`,
-      )
-    }
-  }
-}
+//       return {
+//         ...state,
+//         [networkId]: {
+//           ...(safeAccess(state, [networkId]) || {}),
+//           [hash]: {
+//             ...(safeAccess(state, [networkId, hash]) || {}),
+//             [RECEIPT]: receipt,
+//           },
+//         },
+//       }
+//     }
+//     default: {
+//       throw Error(
+//         `Unexpected action type in TransactionsContext reducer: '${type}'.`,
+//       )
+//     }
+//   }
+// }
 
-export default function Provider({ children }) {
-  const [state, dispatch] = useReducer(reducer, {})
+// export default function Provider({ children }) {
+//   const [state, dispatch] = useReducer(reducer, {})
 
-  const add = useCallback((networkId, hash, response) => {
-    dispatch({ type: ADD, payload: { networkId, hash, response } })
-  }, [])
-  const check = useCallback((networkId, hash, blockNumber) => {
-    dispatch({ type: CHECK, payload: { networkId, hash, blockNumber } })
-  }, [])
-  const finalize = useCallback((networkId, hash, receipt) => {
-    dispatch({ type: FINALIZE, payload: { networkId, hash, receipt } })
-  }, [])
+//   const add = useCallback((networkId, hash, response) => {
+//     dispatch({ type: ADD, payload: { networkId, hash, response } })
+//   }, [])
+//   const check = useCallback((networkId, hash, blockNumber) => {
+//     dispatch({ type: CHECK, payload: { networkId, hash, blockNumber } })
+//   }, [])
+//   const finalize = useCallback((networkId, hash, receipt) => {
+//     dispatch({ type: FINALIZE, payload: { networkId, hash, receipt } })
+//   }, [])
 
-  const value = useMemo(() => [state, { add, check, finalize }], [
-    state,
-    add,
-    check,
-    finalize,
-  ])
+//   const value = useMemo(() => [state, { add, check, finalize }], [
+//     state,
+//     add,
+//     check,
+//     finalize,
+//   ])
 
-  return (
-    <TransactionsContext.Provider value={value}>
-      {children}
-    </TransactionsContext.Provider>
-  )
-}
+//   return (
+//     <TransactionsContext.Provider value={value}>
+//       {children}
+//     </TransactionsContext.Provider>
+//   )
+// }
 
-export function Updater() {
-  const { networkId, library } = useWeb3Context()
+// export function Updater() {
+//   const { networkId, library } = useWeb3Context()
 
-  const globalBlockNumber = useBlockNumber()
+//   const globalBlockNumber = useBlockNumber()
 
-  const [state, { check, finalize }] = useTransactionsContext()
-  const allTransactions = safeAccess(state, [networkId]) || {}
+//   const [state, { check, finalize }] = useTransactionsContext()
+//   const allTransactions = safeAccess(state, [networkId]) || {}
 
-  useEffect(() => {
-    if ((networkId || networkId === 0) && library) {
-      let stale = false
-      Object.keys(allTransactions)
-        .filter(
-          hash =>
-            !allTransactions[hash][RECEIPT] &&
-            allTransactions[hash][BLOCK_NUMBER_CHECKED] !== globalBlockNumber,
-        )
-        .forEach(hash => {
-          library.eth
-            .getTransactionReceipt(hash)
-            .then(receipt => {
-              if (!stale) {
-                if (!receipt) {
-                  check(networkId, hash, globalBlockNumber)
-                } else {
-                  finalize(networkId, hash, receipt)
-                }
-              }
-            })
-            .catch(() => {
-              check(networkId, hash, globalBlockNumber)
-            })
-        })
+//   useEffect(() => {
+//     if ((networkId || networkId === 0) && library) {
+//       let stale = false
+//       Object.keys(allTransactions)
+//         .filter(
+//           hash =>
+//             !allTransactions[hash][RECEIPT] &&
+//             allTransactions[hash][BLOCK_NUMBER_CHECKED] !== globalBlockNumber,
+//         )
+//         .forEach(hash => {
+//           library.eth
+//             .getTransactionReceipt(hash)
+//             .then(receipt => {
+//               if (!stale) {
+//                 if (!receipt) {
+//                   check(networkId, hash, globalBlockNumber)
+//                 } else {
+//                   finalize(networkId, hash, receipt)
+//                 }
+//               }
+//             })
+//             .catch(() => {
+//               check(networkId, hash, globalBlockNumber)
+//             })
+//         })
 
-      return () => {
-        stale = true
-      }
-    }
-  }, [networkId, library, allTransactions, globalBlockNumber, check, finalize])
+//       return () => {
+//         stale = true
+//       }
+//     }
+//   }, [networkId, library, allTransactions, globalBlockNumber, check, finalize])
 
-  return null
-}
+//   return null
+// }
 
-export function useTransactionAdder() {
-  const { networkId } = useWeb3Context()
+// export function useTransactionAdder() {
+//   const { networkId } = useWeb3Context()
 
-  const [, { add }] = useTransactionsContext()
+//   const [, { add }] = useTransactionsContext()
 
-  return useCallback(
-    (response, customData = {}) => {
-      if (!(networkId || networkId === 0)) {
-        throw Error(`Invalid networkId '${networkId}`)
-      }
+//   return useCallback(
+//     (response, customData = {}) => {
+//       if (!(networkId || networkId === 0)) {
+//         throw Error(`Invalid networkId '${networkId}`)
+//       }
 
-      const hash = safeAccess(response, ['hash'])
+//       const hash = safeAccess(response, ['hash'])
 
-      if (!hash) {
-        throw Error('No transaction hash found.')
-      }
-      add(networkId, hash, { ...response, [CUSTOM_DATA]: customData })
-    },
-    [networkId, add],
-  )
-}
+//       if (!hash) {
+//         throw Error('No transaction hash found.')
+//       }
+//       add(networkId, hash, { ...response, [CUSTOM_DATA]: customData })
+//     },
+//     [networkId, add],
+//   )
+// }
 
-export function useAllTransactions() {
-  const { networkId } = useWeb3Context()
+// export function useAllTransactions() {
+//   const { networkId } = useWeb3Context()
 
-  const [state] = useTransactionsContext()
+//   const [state] = useTransactionsContext()
 
-  return safeAccess(state, [networkId]) || {}
-}
+//   return safeAccess(state, [networkId]) || {}
+// }
 
-export function usePendingApproval(tokenAddress) {
-  const allTransactions = useAllTransactions()
+// export function usePendingApproval(tokenAddress) {
+//   const allTransactions = useAllTransactions()
 
-  return (
-    Object.keys(allTransactions).filter(hash => {
-      if (allTransactions[hash][RECEIPT]) {
-        return false
-      } else if (!allTransactions[hash][RESPONSE]) {
-        return false
-      } else if (
-        allTransactions[hash][RESPONSE][CUSTOM_DATA].approval !== tokenAddress
-      ) {
-        return false
-      } else {
-        return true
-      }
-    }).length >= 1
-  )
-}
+//   return (
+//     Object.keys(allTransactions).filter(hash => {
+//       if (allTransactions[hash][RECEIPT]) {
+//         return false
+//       } else if (!allTransactions[hash][RESPONSE]) {
+//         return false
+//       } else if (
+//         allTransactions[hash][RESPONSE][CUSTOM_DATA].approval !== tokenAddress
+//       ) {
+//         return false
+//       } else {
+//         return true
+//       }
+//     }).length >= 1
+//   )
+// }
