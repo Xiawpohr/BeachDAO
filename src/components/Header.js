5651d5ce293cacc520a42c787c6e421361bf6c29
HSIAO PO WEN <xiawpohr@gmail.com>
2019-10-19 22:55:43 +0800

    feat: add Header

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 4.5rem;
+  padding: 0 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const HeaderLogo = styled.div`
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.navyBlue};
+  letter-spacing: 1px;
+`
+
+const HeaderActions = styled.div`
+  display: flex;
+  align-items: center;
+
+  > *:not(:first-child) {
+    margin-left: 2.5rem;
+  }
+`
+
+const HeaderButton = styled.button`
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.navyBlue};
+  background-color: transparent;
+  cursor: pointer;
+  
+  &:focus {
+    outline: none;
+  }
+`
+
+export default function Header() {
+  return (
+    <HeaderWrapper>
+      <HeaderLogo>BeachDAO</HeaderLogo>
+      <HeaderActions>
+        <HeaderButton>發起淨灘</HeaderButton>
+        <HeaderButton>捐款</HeaderButton>
+        <HeaderButton>連線到錢包</HeaderButton>
+      </HeaderActions>
+    </HeaderWrapper>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useBlockNumber } from '../contexts/application'
 import { useETHBalance, useBCDBalance, useBCDTotalSupply } from '../hooks/ethereum'
 import {
@@ -7,6 +8,12 @@ import {
   useDAOProposals,
 } from '../hooks/dao'
 import { amountFormatter } from '../utils'
+import Header from '../components/Header'
+
+const AppWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 
 export default function Home() {
   const blockNumber = useBlockNumber()
@@ -18,7 +25,8 @@ export default function Home() {
   const proposals = useDAOProposals()
 
   return (
-    <>
+    <AppWrapper>
+      <Header />
       <div>Block Number: {blockNumber}</div>
       <div>ETH Balance: {ETHBalance ? amountFormatter(ETHBalance, 18) : '-'}</div>
       <div>BCD Balance: {BCDBalance ? amountFormatter(BCDBalance, 18) : '-'}</div>
@@ -28,6 +36,6 @@ export default function Home() {
       <div>Proposal Locations: {
         proposals.map((proposal, i) => <div key={i}>{proposal.location}</div>)
       }</div>
-    </>
+    </AppWrapper>
   )
 }
